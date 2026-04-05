13852cb5abc6d67d85951ef780597bd220665362
abmusic187-bit <abmusic187@gmail.com>
2026-04-05 07:34:11 +0500

    Update missions.js

--- js/missions.js
+++ js/missions.js
@@ -1,4 +1,15 @@
 import { supabase } from './supabase.js';
+
+export async function claimDaily(userId) {
+    const { data, error } = await supabase.rpc('claim_daily_bonus', { user_id_uuid: userId });
+    
+    if (error) {
+        console.error(error);
+        return { success: false, message: "System Offline" };
+    }
+    return data;
+}    
+import { supabase } from './supabase.js';
 export async function getTasks(userId) {
     return await supabase.from('user_tasks').select('*').gt('remaining', 0).neq('creator_id', userId);
 }
